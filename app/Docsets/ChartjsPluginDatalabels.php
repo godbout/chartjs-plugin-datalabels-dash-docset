1f028a4559a114f63022074e2ede3b1e1b7c620a
Guill Lo <guill.bout@gmail.com>
2020-08-09 00:53:05 +0800

    add guide and section entries

--- app/Docsets/ChartjsPluginDatalabels.php
+++ app/Docsets/ChartjsPluginDatalabels.php
@@ -14,51 +14,12 @@ class ChartjsPluginDatalabels extends BaseDocset
     public const NAME = 'chartjs-plugin-datalabels';
     public const URL = 'chartjs-plugin-datalabels.netlify.app';
     public const INDEX = 'guide/index.html';
-    public const PLAYGROUND = 'https://codepen.io/lucusc/pen/pgQRdd';
+    public const PLAYGROUND = '';
     public const ICON_16 = '../../icons/icon.png';
     public const ICON_32 = '../../icons/icon@2x.png';
     public const EXTERNAL_DOMAINS = [
-        'use.typekit.net',
     ];
 
-
-    public function grab(): bool
-    {
-        $toIgnore = implode('|', [
-            '/cdn-cgi',
-            '/docs/2.9.3',
-            '/docs/master',
-            '/samples/master'
-        ]);
-
-        $toGet = implode('|', [
-            '\.css',
-            '\.ico',
-            '\.js',
-            '\.svg',
-            '/docs',
-            '/samples'
-        ]);
-
-        system(
-            "echo; wget chartjs-plugin-datalabels.netlify.app \
-                --mirror \
-                --trust-server-names \
-                --ignore-case \
-                --page-requisites \
-                --adjust-extension \
-                --convert-links \
-                --span-hosts \
-                --domains={$this->externalDomains()} \
-                --directory-prefix=storage/{$this->downloadedDirectory()} \
-                -e robots=off \
-                --quiet \
-                --show-progress",
-            $result
-        );
-
-        return $result === 0;
-    }
 
     public function entries(string $file): Collection
     {
@@ -76,14 +37,14 @@ class ChartjsPluginDatalabels extends BaseDocset
     {
         $entries = collect();
 
-        if (Str::contains($file, "{$this->url()}/docs/latest/index.html")) {
+        if (Str::contains($file, "{$this->url()}/guide/index.html")) {
             $crawler
-                ->filter('.summary > li.chapter:not(:first-child) a')
+                ->filter('.sidebar-links > li > a')
                 ->each(function (HtmlPageCrawler $node) use ($entries) {
                     $entries->push([
                         'name' => $node->text(),
                         'type' => 'Guide',
-                        'path' => $this->url() . '/docs/latest/' . $this->cleanUrl($node->attr('href')),
+                        'path' => $this->url() . '/guide/' . $node->attr('href'),
                     ]);
                 });
         }
@@ -106,80 +67,10 @@ class ChartjsPluginDatalabels extends BaseDocset
         return $entries;
     }
 
-    /**
-     * Some links are not converted correctly by wget once
-     * the download is finished. no idea why, but this cleans
-     * the few that fail.
-     */
-    protected function cleanUrl($url)
-    {
-        return Str::after($url, 'https://www.chartjs.org/docs/latest/');
-    }
-
     public function format(string $file): string
     {
         $crawler = HtmlPageCrawler::create(Storage::get($file));
 
-        $this->removeLeftSidebar($crawler);
-        $this->removeMenuAndSharingButtons($crawler);
-        $this->removeNavigation($crawler);
-        $this->makeContentFullWidth($crawler);
-        $this->removeSearchResults($crawler);
-
-        $this->removeUnwantedCSS($crawler);
-        $this->removeUnwantedJavaScript($crawler);
-
-        $this->insertDashTableOfContents($crawler);
-
         return $crawler->saveHTML();
-    }
-
-    protected function removeLeftSidebar(HtmlPageCrawler $crawler)
-    {
-        $crawler->filter('.book-summary')->remove();
-    }
-
-    protected function removeMenuAndSharingButtons(HtmlPageCrawler $crawler)
-    {
-        $crawler->filter('body')->after(
-            "<script>$(document).ready(function () { $('.pull-right.js-toolbar-action, .pull-left.btn').hide(); });</script>"
-        );
-    }
-
-    protected function removeNavigation(HtmlPageCrawler $crawler)
-    {
-        $crawler->filter('.navigation')->remove();
-    }
-
-    protected function makeContentFullWidth(HtmlPageCrawler $crawler)
-    {
-        $crawler->filter('.book-body')->setStyle('left', '0px !important');
-    }
-
-    protected function removeSearchResults(HtmlPageCrawler $crawler)
-    {
-        $crawler->filter('.search-results')->remove();
-    }
-
-    protected function removeUnwantedCSS(HtmlPageCrawler $crawler)
-    {
-        $crawler->filter('link[href*="search.css"]')->remove();
-    }
-
-    protected function removeUnwantedJavaScript(HtmlPageCrawler $crawler)
-    {
-        $crawler->filter('script[src*="search.js"]')->remove();
-    }
-
-    protected function insertDashTableOfContents(HtmlPageCrawler $crawler)
-    {
-        $crawler->filter('.page-inner')
-            ->before('<a name="//apple_ref/cpp/Section/Top" class="dashAnchor"></a>');
-
-        $crawler->filter('h2, h3')->each(static function (HtmlPageCrawler $node) {
-            $node->before(
-                '<a id="' . Str::slug($node->text()) . '" name="//apple_ref/cpp/Section/' . rawurlencode($node->text()) . '" class="dashAnchor"></a>'
-            );
-        });
     }
 }
